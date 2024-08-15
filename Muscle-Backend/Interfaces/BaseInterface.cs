f749f1ce33ca1f3eb3e833b93e8b90522bff6c71
kamorin-19 <159443759+kamorin-19@users.noreply.github.com>
2024-08-15 13:53:06 +0900

    feat:基本になるインターフェースを追加

---
+++ Muscle-Backend/Interfaces/BaseInterface.cs
@@ -0,0 +1,26 @@
+﻿namespace Muscle_Backend.Interfaces
+{
+    public interface BaseInterface
+    {
+        /// <summary>
+        /// データ取得
+        /// </summary>
+        public IEnumerable<T> SelectRecords<T>();
+
+        /// <summary>
+        /// データ作成
+        /// </summary>
+        public void InsertRecord();
+
+        /// <summary>
+        /// データ更新
+        /// </summary>
+        public void UpdateRecord();
+
+        /// <summary>
+        /// データ削除
+        /// </summary>
+        public void DeleteRecord();
+
+    }
+}
